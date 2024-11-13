5a7ef1546181596fd91b650176fa1499d42a0f69
Mateo Nicolás Raffin <raffinmateonicolas@gmail.com>
2024-11-13 17:00:01 -0300

    proyecto base parte02

---
+++ project-clases/src/saludo.jsx
@@ -0,0 +1,18 @@
+function Saludo() {
+    return(
+    
+        <div className="cont-title" >
+            <nav className="navBar">
+                <ul className="nav-list">
+                    <li className="item">Productos</li>
+                    <li className="item">Hablemos</li>
+                </ul>
+            </nav>
+            <h2 className="title-h2">¡Welcome to React Experience!</h2>
+            <p className="parag-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor velit cum deleniti, nemo numquam porro repellat perferendis eveniet odit sint consequuntur id ea qui deserunt odio eos obcaecati veritatis minima.</p>
+        </div>
+        
+    )
+}
+
+export default Saludo
