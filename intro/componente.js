bca0d71ddf77e811d5d18b1aa1148b8ed2964804
Mateo Nicolás Raffin <raffinmateonicolas@gmail.com>
2024-11-13 10:07:21 -0300

    proyecto base-clase01

---
+++ intro/componente.js
@@ -0,0 +1,11 @@
+const messageElement = document.createElement('h1');
+messageElement.textContent = 'Hola, Bienvenidos';
+
+const button = document.createElement('button');
+button.textContent = 'Cambiar mensaje';
+button.addEventListener('click', ()=>{
+    messageElement.textContent = 'Hiciste click!'
+});
+
+document.body.appendChild(messageElement);
+document.body.appendChild(button);
